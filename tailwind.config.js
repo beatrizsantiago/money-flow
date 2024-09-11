5ecb24b858e409104f5b5fb34c591c561de34e10
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2024-09-11 20:17:15 -0300

    🔧 Add custom theme to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        main: '#004D61',
+      },
+      secondary: {
+        main: '#47A138',
+        light: '#E4EDE3',
+      },
+      gray: {
+        main: '#8B8B8B',
+        extraLight: '#F8F8F8',
+        light: '#CCCCCC',
+        medium: '#CBCBCB',
+        dark: '#444444',
+        blueGray: '#DEE9EA',
+      },
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        'blue-white-gradient': 'linear-gradient(180deg, #004D61 0%, #FFFFFF 100%)',
+      },
+      fontSize: {
+        sm: '0.812rem',
+        md: '1rem',
+        lg: '1.25rem',
+        xl: '1.568rem',
+      },
+    },
   },
   plugins: [],
 }
