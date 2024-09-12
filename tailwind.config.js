66fc2b991305132902ae2dd7ca72d6a444e589e1
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2024-09-11 23:12:53 -0300

    🔧 Refactor tailwind.config.js to update color palette and add custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      primary: {
-        main: '#004D61',
-      },
-      secondary: {
-        main: '#47A138',
-        light: '#E4EDE3',
-      },
-      gray: {
-        main: '#8B8B8B',
-        extraLight: '#F8F8F8',
-        light: '#CCCCCC',
-        medium: '#CBCBCB',
-        dark: '#444444',
-        blueGray: '#DEE9EA',
-      },
-    },
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
     },
     extend: {
+      colors: {
+        primary: {
+          main: '#004D61',
+        },
+        secondary: {
+          main: '#47A138',
+          light: '#E4EDE3',
+        },
+        gray: {
+          main: '#8B8B8B',
+          xlight: '#F8F8F8',
+          light: '#CCCCCC',
+          medium: '#CBCBCB',
+          dark: '#444444',
+          bluegray: '#DEE9EA',
+        },
+      },
       backgroundImage: {
         'blue-white-gradient': 'linear-gradient(180deg, #004D61 0%, #FFFFFF 100%)',
       },
