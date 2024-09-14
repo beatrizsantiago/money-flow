2559235bde17a94164527f3717d3ea03d79808e3
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2024-09-14 15:27:29 -0300

    ✨ Add Input component and stories

---
+++ src/components/Input.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  color?: "primary" | "secondary";
+  error?: boolean;
+};
+
+const COLOR = {
+  primary: "border-primary-main",
+  secondary: "border-secondary-main",
+};
+
+const Input = ({
+  color = "primary",
+  error = false,
+  className = "",
+  ...rest
+}:InputProps) => (
+  <input
+    className={`
+      px-4 h-12 border rounded-lg text-gray-dark font-normal focus:outline-none
+      ${error ? 'border-red-500' : COLOR[color]}
+      ${className}
+    `}
+    {...rest}
+  />
+);
+
+export default Input;
---
+++ src/stories/Input.stories.ts
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+ 
+import Input from '../components/Input';
+ 
+const meta: Meta<typeof Input> = {
+  component: Input,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+ 
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    placeholder: 'Primary Input',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    placeholder: 'Secondary Input',
+    color: 'secondary',
+  },
+};
+
+export const Error: Story = {
+  args: {
+    placeholder: 'Error Input',
+    error: true,
+  },
+};
